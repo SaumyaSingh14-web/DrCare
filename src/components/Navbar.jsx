031faab343b10f1f1259225430a7592d6d8e9eab
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-01-05 20:04:00 +0530

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(   
+        <div>
+            <ul className="bg-white h-3 w-2 text-blue-300 flex font-semibold hover:capitalize">
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
