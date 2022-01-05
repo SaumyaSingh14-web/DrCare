895a22203b165c6765c74bb6862d3864b6cee9e1
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-01-05 20:15:50 +0530

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ function Navbar(){
             <ul className="bg-white h-3 w-2 text-blue-300 flex font-semibold hover:capitalize">
                 <li>Home</li>
                 <li>About</li>
+                <li>contact</li>
             </ul>
         </div>
     );
