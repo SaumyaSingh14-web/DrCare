b5871ad45199e43ea91c36ada8e7ca82ac0493ac
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-01-22 23:32:17 +0530

    commit

--- src/components/Allcards.jsx
+++ src/components/Allcards.jsx
@@ -4,9 +4,9 @@ function Allcards(){
     return(
         <div className="md:flex md:ml-12 md:mr-12">
         <Cards title= "Qualified Doctors" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic." className = "bg-blue-400"/>
-        <Cards title= "Qualified Doctors" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic."/>
-        <Cards title= "Qualified Doctors" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic." className = "bg-blue-400"/>
-        <Cards title= "Qualified Doctors" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic."/>
+        <Cards title= "Emergency Care" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic."/>
+        <Cards title= "Outdoor Checkup" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic." className = "bg-blue-400"/>
+        <Cards title= "24 Hours Service" description = "even all-powerful pointing has no control about the blind texts it is almost an unorthoraphic."/>
         </div>
     );
 }
